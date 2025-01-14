ce8026f81eb4b176f35a732220383b5d67470489
priyabrata das <pdas544@gmail.com>
2025-01-14 20:34:12 +0530

    add flash message and validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,16 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     //
     public function register(Request $request){
         $data = $request->validate([
-            'username' => 'required',
-            'email'=>'required',
-            'password'=>'required'
+            'username' => ['required','min:3',Rule::unique('users','username')],
+            'email'=>['required','email',Rule::unique('users','email')],
+            'password'=>['required','min:5','confirmed']
         ]);
+
+        $data['password'] = bcrypt($data['password']);
 
         User::create($data);
 
