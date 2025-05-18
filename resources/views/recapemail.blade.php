8c863a7558140e1a4bcd4c0d11549ac1b2bb8a47
priyabrata das <pdas544@gmail.com>
2025-05-18 22:46:43 +0530

    add schedule funcationality for email using Schedule facade

---
+++ resources/views/recapemail.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <p> Here is a quick recap of the application</p>
+    <p>Post count: <strong>{{$postCount}}</strong></p>
+    <p>User count: <strong>{{$userCount}}</strong></p>
+</div>
--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Mail\RecapEmail;
 use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+//schedule an email, use Schedule facade
+Schedule::call(function(){
+    Mail::to('test@google.com')->send(new RecapEmail());
+})->everyMinute();
